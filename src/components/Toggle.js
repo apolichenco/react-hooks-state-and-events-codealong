089ca6362db7babcbce068a2ea37312fd4de6467
abraham <apolichenco@gmail.com>
2022-07-19 20:17:53 -0400

    commit tree 57b397d933f49b28c59a4e5eb89b142ecb7747fd

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React , { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  const [isOn, setIsOn] = useState(false)
+
+  return <button>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
